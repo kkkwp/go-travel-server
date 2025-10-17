7831d66a85151fd4d9c8ead43edc5e10832a57c5
kkkwp <chxrryda@gmail.com>
2025-10-17 15:59:15 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/travelers/gotravelserver/GoTravelServerApplication.java
@@ -0,0 +1,13 @@
+package com.travelers.gotravelserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoTravelServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoTravelServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelers/gotravelserver/GoTravelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelers.gotravelserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoTravelServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
